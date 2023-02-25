d8c4776c4409e344452a21f4c7dc515d96a03d2b
JagathA <jagath.ariy@gmail.com>
2023-02-25 17:13:01 +0000

    Added Not Found component

--- client/src/components/fakelandia-router.tsx
+++ client/src/components/fakelandia-router.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './home';
 import Confession from './confession';
 import Misdemeanour from './misdemeanour';
+import NotFound from './not_found';
 
 export const FakeLandiaRouter: React.FC = () =>
     <>
         <Routes>
-            <Route path = '/' element = {<Home/>}/>
-            <Route path = '/confession' element = {<Confession/>}/>
-            <Route path = '/misdemeanour' element = {<Misdemeanour/>}/>
+            <Route path='/' element={<Home />} />
+            <Route path='/confession' element={<Confession />} />
+            <Route path='/misdemeanour' element={<Misdemeanour />} />
+            <Route path='*' element={<NotFound />} />
+
         </Routes>
     </>
 
---
+++ client/src/components/not_found.tsx
@@ -0,0 +1,7 @@
+const NotFound: React.FC = () =>
+<>
+    <h2 className='subtitle has-text-danger'>404!</h2>
+    <h2 className='subtitle has-text-danger'>PAGE NOT FOUND!</h2>
+</>;
+
+export default NotFound;
