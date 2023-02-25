9de87d4e8852759cd26f387f5f5a4da709324351
JagathA <jagath.ariy@gmail.com>
2023-02-25 17:03:03 +0000

    Initial version

---
+++ client/src/components/home.tsx
@@ -0,0 +1,11 @@
+
+const Home: React.FC = () =>
+    <>
+        <section>
+            <h2 className='subtitle'>Home.</h2>
+            <p>I have been a fan of <em>React Router</em> ever since I heard their first album 😍😍</p>
+            <p>Please enjoy my fan page! I'll make more content soon.</p>
+        </section>
+    </>
+
+export default Home;
---
+++ client/src/components/misdemeanour.tsx
@@ -0,0 +1,11 @@
+
+const Misdemeanour: React.FC = () =>
+    <>
+        <section>
+            <h2 className='subtitle'>Misdemeanour</h2>
+            <p>I have been a fan of <em>React Router</em> ever since I heard their first album 😍😍</p>
+            <p>Please enjoy my fan page! I'll make more content soon.</p>
+        </section>
+    </>;
+
+export default Misdemeanour;
