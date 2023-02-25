26b4249b22f59fb4b86fe4b87b681094268e57fd
JagathA <jagath.ariy@gmail.com>
2023-02-25 16:53:55 +0000

    Added Browse Router. removed junk.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,36 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { FakeLandiaRouter } from './components/fakelandia-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <BrowserRouter>
+        <FakeLandiaRouter />
+      </BrowserRouter>
+
+    </>
+
   );
 }
 
 export default App;
+
+// <div className="App">
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
