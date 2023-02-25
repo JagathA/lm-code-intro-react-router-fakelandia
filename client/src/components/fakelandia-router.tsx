dc3158db16fd2865cf43d9d0712d784364648bcd
JagathA <jagath.ariy@gmail.com>
2023-02-25 20:18:57 +0000

    Added MainLayout

--- client/src/components/fakelandia-router.tsx
+++ client/src/components/fakelandia-router.tsx
@@ -3,15 +3,17 @@ import Home from './home';
 import Confession from './confession';
 import Misdemeanour from './misdemeanour';
 import NotFound from './not_found';
+import MainLayout from './main_layout';
 
 export const FakeLandiaRouter: React.FC = () =>
     <>
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/confession' element={<Confession />} />
-            <Route path='/misdemeanour' element={<Misdemeanour />} />
-            <Route path='*' element={<NotFound />} />
-
+            <Route path='/' element={<MainLayout />} >
+                <Route index element={<Home />} />
+                <Route path='confession' element={<Confession />} />
+                <Route path='misdemeanour' element={<Misdemeanour />} />
+                <Route path='*' element={<NotFound />} />
+            </Route>
         </Routes>
     </>
 
