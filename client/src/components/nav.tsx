5354452a0c93e117a394104e9b51e8f75f839959
JagathA <jagath.ariy@gmail.com>
2023-03-01 20:41:04 +0000

    added css classs to Nav

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-const Nav = () => <nav className='is-flex is-flex-grow-1 is-align-items-center' style={{width:'100%'}}>
+const Nav = () => <nav className='is-flex is-flex-grow-1 is-align-items-center sticky' style={{width:'100%'}}>
     <ul  style={{width:'100%'}}
             className='is-flex is-flex-direction-row is-justify-content-space-evenly is-justify-content-space-evenly'>
         <li><NavLink to='/'>Home</NavLink></li>
