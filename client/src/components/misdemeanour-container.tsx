a76488a06af320aeb38aef3fe93e896b9472f840
JagathA <jagath.ariy@gmail.com>
2023-03-01 20:38:02 +0000

    Changed oredr of table headers

--- client/src/components/misdemeanour-container.tsx
+++ client/src/components/misdemeanour-container.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import { Misdemeanour } from "../types/misdemeanours.types";
+import { Misdemeanour, MisdemeanourKind } from "../types/misdemeanours.types";
 import MisdemeanourRow from './misdemeanour';
 
 const MisdemeanourContainer: React.FC = () => {
@@ -8,28 +8,26 @@ const MisdemeanourContainer: React.FC = () => {
     const [misdemeanours, setMisdemeanours] = useState<Array<Misdemeanour>>([]);
 
     useEffect(() => {
-        getMisdemeanours(100);
+        getMisdemeanours(500);
     }, []);
 
     const getMisdemeanours = async (number: number) => {
 
         const apiResponse = await fetch(`http://localhost:8080/api/misdemeanours/${number}`);
         const json = await apiResponse.json();
-        //  as { data: Misdemeanour[] };
         setMisdemeanours(json.misdemeanours);
-        console.log("*************** =>")
         console.log(json.misdemeanours);
     };
 
     const buildRows = () => {
 
-        // we'll need arrays to store the rows and cols in, and they will be of type JSX.Element
+        // we'll need arrays to store the rows in, and they will be of type JSX.Element
         let rows: Array<JSX.Element> = [];
 
         misdemeanours.forEach((item, index) => {
             rows.push(
                 <MisdemeanourRow key={index}
-                    misdemeanour={item}
+                citizenId = {item.citizenId} date = {item.date}  misdemeanour = {item.misdemeanour}
                 />
             );
         })
@@ -41,20 +39,17 @@ const MisdemeanourContainer: React.FC = () => {
     return (
         <table>
             <thead>
-            <tr className="red">
-                    <th>Citizen ID</th>
-                    <th>Misdemeanour</th>
-                    <th>Date</th>
-                
+                <tr className="red">
+                    <th scope="col">Citizen ID</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Misdemeanour</th>
+
                 </tr>
             </thead>
             <tbody>
                 {buildRows()}
             </tbody>
         </table>
-        // <div className="misdeameanour-container">
-        //     {buildRows()}
-        // </div>)
     )
 };
 
