485e236d9f54fe82b046ff763d9fbb3dd5b01794
JagathA <jagath.ariy@gmail.com>
2023-03-01 20:41:47 +0000

    misdeameanour test values changed

--- client/src/components/misdemeanour.test.tsx
+++ client/src/components/misdemeanour.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, } from '@testing-library/react';
-import MisdemeanourRow from './misdemeanour'
+import MisdemeanourRow, {misdeameanourDisplay} from './misdemeanour'
 import {Misdemeanour} from '../types/misdemeanours.types'
 
 describe("<MisdemeanourRow>", () => {
@@ -13,9 +13,10 @@ describe("<MisdemeanourRow>", () => {
         misdemeanour: "rudeness",
         date: "12/03/2012"
     }
+    const displayedMisdeameanour = misdeameanourDisplay["rudeness"];
     render(<MisdemeanourRow {...requiredProps} />);
     expect(screen.getByText("100")).toBeInTheDocument();
-    expect(screen.getByText("rudeness")).toBeInTheDocument();
+    expect(screen.getByText(/Mild Public Rudeness =/i)).toBeInTheDocument();
     expect(screen.getByText("12/03/2012")).toBeInTheDocument();
   });
 });
