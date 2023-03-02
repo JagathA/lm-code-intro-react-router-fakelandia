2510c4fbdfab64a330c029010e91e7092cb1ea51
JagathA <jagath.ariy@gmail.com>
2023-03-02 20:00:46 +0000

    Revert "Added Filter Component"
    This reverts commit b60a0ab4a7eb1fd3b3a1596895c30541cfad317c.

--- client/src/components/misdemeanour-container.tsx
+++ client/src/components/misdemeanour-container.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import { Misdemeanour } from "../types/misdemeanours.types";
+import { Misdemeanour, MisdemeanourKind } from "../types/misdemeanours.types";
 import MisdemeanourRow from './misdemeanour';
-import MisdeameanourFilter, { misdeameanourFilter } from './misdeameanour-filter';
 
 const MisdemeanourContainer: React.FC = () => {
 
     const [misdemeanours, setMisdemeanours] = useState<Array<Misdemeanour>>([]);
-    const [filter, setFilter] = useState<misdeameanourFilter>('none');
 
     useEffect(() => {
         getMisdemeanours(500);
@@ -23,14 +21,13 @@ const MisdemeanourContainer: React.FC = () => {
 
     const buildRows = () => {
 
+        // we'll need arrays to store the rows in, and they will be of type JSX.Element
         let rows: Array<JSX.Element> = [];
 
-        const filteredMisdemeanours = misdemeanours.filter(item => ((item.misdemeanour === filter) || (filter === "none")));
-
-        filteredMisdemeanours.forEach((item, index) => {
+        misdemeanours.forEach((item, index) => {
             rows.push(
                 <MisdemeanourRow key={index}
-                    citizenId={item.citizenId} date={item.date} misdemeanour={item.misdemeanour}
+                citizenId = {item.citizenId} date = {item.date}  misdemeanour = {item.misdemeanour}
                 />
             );
         })
@@ -46,13 +43,6 @@ const MisdemeanourContainer: React.FC = () => {
                     <th scope="col">Citizen ID</th>
                     <th scope="col">Date</th>
                     <th scope="col">Misdemeanour</th>
-
-                </tr>
-                <tr className="red">
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-
-                    <th scope="col"><MisdeameanourFilter filter={filter} onChange={(newValue) => setFilter(newValue)} /></th>
 
                 </tr>
             </thead>
