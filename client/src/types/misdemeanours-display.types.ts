3bcb42a26c23f49fe8a393a324635e788216b4f1
JagathA <jagath.ariy@gmail.com>
2023-03-04 21:28:26 +0000

    Initial commit

---
+++ client/src/types/misdemeanours-display.types.ts
@@ -0,0 +1,11 @@
+import { MisdemeanourKind } from "../types/misdemeanours.types";
+ 
+export const misdemeanourDisplay : { [key in MisdemeanourKind]: string} =
+{
+    "rudeness": `Mild Public Rudeness = 🤪`,
+    "vegetables": `Not Eating Your Vegetables = 🥗`,
+    "lift": `Speaking in a Lift = 🗣`,
+    "united": ` Supporting Manchester United = 😈`
+};
+
+export type Reason = MisdemeanourKind | 'I just want to talk';
