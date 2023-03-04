15f0f28960741a5065168dc73cdd4da5ae3f9aa9
JagathA <jagath.ariy@gmail.com>
2023-03-04 19:57:03 +0000

    Tidy up

--- client/src/components/misdemeanour.test.tsx
+++ client/src/components/misdemeanour.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, } from '@testing-library/react';
 import MisdemeanourRow,{MisdemeanourProps}from './misdemeanour'
-import {Misdemeanour} from '../types/misdemeanours.types'
 
 describe("<MisdemeanourRow>", () => {
 
