ee724b0fc8b80ce43807c9363072cb009f299668
JagathA <jagath.ariy@gmail.com>
2023-03-04 20:00:47 +0000

    Added error handling

--- client/src/components/confession/subject.tsx
+++ client/src/components/confession/subject.tsx
@@ -1,50 +1,49 @@
 import React from 'react';
 import { useState } from 'react';
-//import ErrorMessage from './ErrorMessage';
+import ErrorMessage from './ErrorMessage';
 
 export interface SubjectProps {
 	subject: string;
 	onChange: (newValue: string) => void;
 }
 
-// const errLbl = "ERROR:"
-// const SPECIES_NAME_MIN_LENGTH = 3;
-// const SPECIES_NAME_MAX_LENGTH = 23;
+const errLbl = "ERROR:"
+const SUBJECT_MIN_LENGTH = 3;
+const SUBJECT_MAX_LENGTH = 25;
 
-// export const errorSpeciesNameTooShort = `${errLbl} Species Name must be minimum ${SPECIES_NAME_MIN_LENGTH} characters`
-// export const errorSpeciesNameTooLong = `${errLbl} Species Name must be maximum ${SPECIES_NAME_MAX_LENGTH} characters`
-// export const errorSpeciesNameInvalidCharacters = `${errLbl} No numbers or special characters allowed!`
+export const errorSubjectTooShort = `${errLbl} Subject Name must be minimum ${SUBJECT_MIN_LENGTH} characters`
+export const errorSubjectTooLong = `${errLbl} Subject Name must be maximum ${SUBJECT_MAX_LENGTH} characters`
 
 const Subject: React.FC<SubjectProps> = ({ subject, onChange }) => {
 
-	// const [errorMessage, setErrorMessage] =
-	// 	useState<string | undefined>(undefined);
+	const [errorMessage, setErrorMessage] =
+		useState<string | undefined>(undefined);
 
-	// const validate: (value: string) => string | undefined = (value) => {
-	// 	const reg = /^[a-z]+$/i;;
-	// 	if (value.length > 0) {
-	// 		if (!reg.test(value)) {
-	// 			return errorSubjectInvalidCharacters;
-	// 		}
-	// 		else if (value.length < SPECIES_NAME_MIN_LENGTH) {
-	// 			return errorSubjectTooShort;
-	// 		}
-	// 	}
+	const validate: (value: string) => string | undefined = (value) => {
+		
+		if (value.length > 0) {
+		 if (value.length < SUBJECT_MIN_LENGTH) {
+				return errorSubjectTooShort;
+			}
+			else if (value.length > SUBJECT_MAX_LENGTH) {
+				return errorSubjectTooLong;
+			}
+		}
 
-	// 	return undefined;
-	// }
+		return undefined;
+	}
 	return (
 		<>
 			<p>
 				<label htmlFor='subject'>Subject</label>
 				<input id='subject' type='text' value={subject} onChange={
 					(e) => {
-						// const errorMessage = validate(e.target.value);
-						// setErrorMessage(errorMessage);
+						const errorMessage = validate(e.target.value);
+						setErrorMessage(errorMessage);
 						onChange(e.target.value)
 					}}
 				/>
-				{/* <ErrorMessage errorMessage={errorMessage} /> */}
+				<ErrorMessage errorMessage={errorMessage} />
 			</p>
 		</>
 	)
