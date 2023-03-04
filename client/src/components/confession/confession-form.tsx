8d79efd694e4b1a0fe5be917c54728567e87e24b
JagathA <jagath.ariy@gmail.com>
2023-03-04 21:25:59 +0000

    Reason for contact component added

--- client/src/components/confession/confession-form.tsx
+++ client/src/components/confession/confession-form.tsx
@@ -1,22 +1,20 @@
 import { useState } from 'react';
-import { MisdemeanourKind } from "../../types/misdemeanours.types";
 import ConfessionHeader from './header';
 import Subject from './subject';
+import ReasonForContact from './reason-for-contact';
+import {Reason} from '../../types/misdemeanours-display.types'
 
-type ReasonForContact = MisdemeanourKind | 'I just want to talk';
 
 
 const ConfessionForm = () => {
 	const [subject, setSubject] = useState<string>('');
-	const [reasonForContact, setreasonForContact] = useState<ReasonForContact>('I just want to talk');
+	const [reasonForContact, setReasonForContact] = useState<Reason>('I just want to talk');
 	const [confess, setConfess] = useState<string>('I an guilty');
 	const [submitted, setSubmitted] = useState<boolean>(false);
-
 
 	function submitClicked() {
 		setSubmitted(true);
 	}
-
 
 	return (
 		<section className='w12MForm'>
@@ -24,17 +22,17 @@ const ConfessionForm = () => {
 			<>
 				<Subject subject={subject} onChange={(newValue) => setSubject(newValue)} />
 			</>
-			{/* <>
-				<TwoPlusTwo twoPlusTwo={twoPlusTwo} onChange={(newValue) => setTwoPlusTwo(newValue)} />
-			</>
-
 			<>
-				<ReasonForSparing reasonForSparing={reasonForSparing} onChange={(newValue) => setReasonForSparing(newValue)} />
+				<ReasonForContact reasonForContact={reasonForContact} onChange={(newValue) => setReasonForContact(newValue)} />
 			</>
 
-			<button type='submit' onClick={() => {
-				submitClicked();
-			}}>Submit</button> */}
+			 {/* <>
+			 	<ReasonForSparing reasonForSparing={reasonForSparing} onChange={(newValue) => setReasonForSparing(newValue)} />
+			 </>
+
+			 <button type='submit' onClick={() => {
+			 	submitClicked();
+			 }}>Submit</button> */} 
 
 			{/* <section>
 				<SubmittedData submitted={submitted} speciesName={speciesName} planetName={planetName} numberOfBeings={numberOfBeings} twoPlusTwo={twoPlusTwo} reasonForSparing={reasonForSparing} />
