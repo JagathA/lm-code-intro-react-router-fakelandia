4a8936577dd0e7368bf1c578744f4a81c02bf2d0
JagathA <jagath.ariy@gmail.com>
2023-03-04 21:28:11 +0000

    Tidy up

--- client/src/components/confession/subject.tsx
+++ client/src/components/confession/subject.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import ErrorMessage from './ErrorMessage';
+import ErrorMessage from './error-message';
 
 export interface SubjectProps {
 	subject: string;
