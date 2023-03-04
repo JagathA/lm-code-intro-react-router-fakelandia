4d864c1214309584af22478f0c1f284c594e1aa5
JagathA <jagath.ariy@gmail.com>
2023-03-04 19:56:39 +0000

    Tidy up

--- client/src/components/fakelandia-router.tsx
+++ client/src/components/fakelandia-router.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
 import Confession from './confession/confession-form';
-import Misdemeanour from './misdemeanour';
 import NotFound from './not_found';
 import MainLayout from './main_layout';
 import MisdemeanourContainer from './misdemeanour-container';
