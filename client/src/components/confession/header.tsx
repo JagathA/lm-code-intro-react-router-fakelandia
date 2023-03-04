0515a4d1c92b0a75f86c5396e6f899a6cea5584d
JagathA <jagath.ariy@gmail.com>
2023-03-04 10:21:10 +0000

    Initial commit

---
+++ client/src/components/confession/header.tsx
@@ -0,0 +1,9 @@
+const ConfessionHeader = () => (
+	<>
+		<h2 className='subtitle'>Confession</h2>
+		<p>It’s very difficult to catch people committing misdemeanours so we appreciate it when citizens confess to us directly</p>
+		<p>However, if you are just having a hard day and need to vent then you’re welcome to contact us here too. up to you !</p>
+	</>
+);
+
+export default ConfessionHeader;
