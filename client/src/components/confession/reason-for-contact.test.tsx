aa164d90cada0f226fb1ad8a1a1748f83842a670
JagathA <jagath.ariy@gmail.com>
2023-03-05 00:09:07 +0000

    Initial version

---
+++ client/src/components/confession/reason-for-contact.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import ReasonForContact, { reasonForContactProps } from './reason-for-contact';
+import user from '@testing-library/user-event';
+
+describe("<ReasonForContact>", () => {
+	const labelText = "Reason For Contact";
+
+	test(`Given the required props, 
+		When the component is rendered
+		Then the label text "Filter" should be present`, () => {
+		const mockChange = jest.fn();
+		const requiredProps: reasonForContactProps = {
+			reasonForContact: "just-talk",
+			onChange: mockChange,
+		}
+		render(<ReasonForContact {...requiredProps} />);
+		expect(screen.getByText(labelText)).toBeInTheDocument();
+	});
+
+	test(`Given the required props, 
+		When the component is rendered
+		Then the vaues passed in props should be present`, () => {
+		const mockChange = jest.fn();
+		const requiredProps: reasonForContactProps = {
+			reasonForContact: "rudeness",
+			onChange: mockChange,
+		}
+		render(<ReasonForContact {...requiredProps} />);
+		expect(screen.getByDisplayValue(requiredProps.reasonForContact)).toBeInTheDocument();
+	});
+
+	test(`Given component is rendered, 
+		when the user selects option "2",
+		Then onChange function is called the corrcet number of times`, async () => {
+		const mockChange = jest.fn();
+		const requiredProps: reasonForContactProps = {
+			reasonForContact: "rudeness",
+			onChange: mockChange,
+		}
+		render(<ReasonForContact {...requiredProps} />);
+		await user.selectOptions(screen.getByLabelText(labelText), "vegetables");
+		expect(requiredProps.onChange).toHaveBeenCalledTimes(1);
+	});
+
+});
