ef65b8816317dbba3ed5d5ee57dfd9d4c3027dbb
JagathA <jagath.ariy@gmail.com>
2023-03-06 19:35:04 +0000

    button enabled on validity flags

--- client/src/components/confession/confession-form.tsx
+++ client/src/components/confession/confession-form.tsx
@@ -2,18 +2,18 @@ import { useState } from 'react';
 import ConfessionHeader from './header';
 import Subject from './subject';
 import ReasonForContact from './reason-for-contact';
-import {Reason} from '../../types/misdemeanours-display.types'
+import { Reason } from '../../types/misdemeanours-display.types'
 import ConfessionText from './confession-text';
 
 
 
 const ConfessionForm = () => {
 	const [subject, setSubject] = useState<string>('');
-    const [subjectValid, setSubjectValid] = useState<boolean>(false);
+	const [subjectValid, setSubjectValid] = useState<boolean>(false);
 	const [reasonForContact, setReasonForContact] = useState<Reason>('just-talk');
-    const [reasonForContactValid, setReasonForContactValid] = useState<boolean>(false);
+	const [reasonForContactValid, setReasonForContactValid] = useState<boolean>(false);
 	const [confessionText, setConfessionText] = useState<string>('I an guilty');
-    const [confessionTextValid, setConfessionTextValid] = useState<boolean>(false);
+	const [confessionTextValid, setConfessionTextValid] = useState<boolean>(false);
 	const [submitted, setSubmitted] = useState<boolean>(false);
 
 	function submitClicked() {
@@ -21,28 +21,24 @@ const ConfessionForm = () => {
 	}
 
 	return (
-		<section className='w12MForm'>
+		<section>
 			<ConfessionHeader />
 			<>
-				<Subject subject={subject} onChange={(newValue,newValidity) => {setSubject(newValue); setSubjectValid(newValidity)}} />
+				<Subject subject={subject} onChange={(newValue, newValidity) => { setSubject(newValue); setSubjectValid(newValidity) }} />
 			</>
 			<>
-				<ReasonForContact reasonForContact={reasonForContact} onChange={(newValue,newValidity) => {setReasonForContact(newValue); setReasonForContactValid(newValidity) }} />
+				<ReasonForContact reasonForContact={reasonForContact} onChange={(newValue, newValidity) => { setReasonForContact(newValue); setReasonForContactValid(newValidity) }} />
 			</>
 
-			 <>
-			 	<ConfessionText confessionText={confessionText} onChange={(newValue,newValidity) => {setConfessionText(newValue); setConfessionTextValid(newValidity)} }/>
-			 </>
+			<>
+				<ConfessionText confessionText={confessionText} onChange={(newValue, newValidity) => { setConfessionText(newValue); setConfessionTextValid(newValidity) }} />
+			</>
 
-			 <button disabled = {!(subjectValid && reasonForContactValid && confessionTextValid)} type='submit' onClick={() => {
-			 	submitClicked();
-			 }}>Submit</button>  
+			<button disabled={!(subjectValid && reasonForContactValid && confessionTextValid)} type='submit' onClick={() => {
+				submitClicked();
+			}}>Submit</button>
 
-			{/* <section>
-				<SubmittedData submitted={submitted} speciesName={speciesName} planetName={planetName} numberOfBeings={numberOfBeings} twoPlusTwo={twoPlusTwo} confessionText={confessionText} />
-			</section> */}
 
-		
 		</section>
 	);
 };
