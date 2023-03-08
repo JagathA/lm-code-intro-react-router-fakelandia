0709d8f46bf7d6646d6c96a7dc5e4687b030cd85
JagathA <jagath.ariy@gmail.com>
2023-03-08 12:49:17 +0000

    functionality to submit new confession

--- client/src/components/confession/confession-form.tsx
+++ client/src/components/confession/confession-form.tsx
@@ -1,23 +1,67 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import ConfessionHeader from './header';
 import Subject from './subject';
 import ReasonForContact from './reason-for-contact';
 import { Reason } from '../../types/misdemeanours-display.types'
 import ConfessionText from './confession-text';
+import ErrorMessage from './error-message';
+import {MisdemeanoursContext, SetMisdemeanoursContext} from '../fakelandia-router'
+import {Misdemeanour} from '../../types/misdemeanours.types'
 
 
 
-const ConfessionForm = () => {
+const ConfessionForm : React.FC = () => {
 	const [subject, setSubject] = useState<string>('');
 	const [subjectValid, setSubjectValid] = useState<boolean>(false);
 	const [reasonForContact, setReasonForContact] = useState<Reason>('just-talk');
 	const [reasonForContactValid, setReasonForContactValid] = useState<boolean>(false);
 	const [confessionText, setConfessionText] = useState<string>('I an guilty');
 	const [confessionTextValid, setConfessionTextValid] = useState<boolean>(false);
-	const [submitted, setSubmitted] = useState<boolean>(false);
+	const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
-	function submitClicked() {
-		setSubmitted(true);
+	const misdemeanours = useContext(MisdemeanoursContext);
+    const setMisdemeanours = useContext(SetMisdemeanoursContext);
+
+	const submitClicked = async () => {
+		setErrorMessage(undefined);
+		const apiResponse = await fetch('http://localhost:8080/api/confess', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"subject": subject,
+				"reason": reasonForContact,
+				"details": confessionText
+			})
+		});
+
+		const json = await apiResponse.json();
+
+		if (json.success) {
+			if ((!json.justTalked) && (reasonForContact !== "just-talk")){
+
+				console.log("add to mis demeanours list");
+				const newMisdemeanour : Misdemeanour = {
+					citizenId : 123456,
+					misdemeanour : reasonForContact,
+					date : "12/12/2040" //new Date().toLocaleDateString()
+				};
+				let misdameanourList = [...misdemeanours];
+				misdameanourList.push(newMisdemeanour);
+				setMisdemeanours(misdameanourList);
+			}
+			else if (((json.justTalked) && (reasonForContact !== "just-talk")) ||
+				    ((!json.justTalked) && (reasonForContact === "just-talk")))   
+			 {
+				setErrorMessage("unexpected misdeamenour type");
+			}
+		}
+		else {
+			setErrorMessage(json.message)
+		}
+
 	}
 
 	return (
@@ -38,7 +82,9 @@ const ConfessionForm = () => {
 				submitClicked();
 			}}>Submit</button>
 
-
+			<>
+				<ErrorMessage errorMessage={errorMessage} />
+			</>
 		</section>
 	);
 };
