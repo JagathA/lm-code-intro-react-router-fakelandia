b7cb506192d884f565bf22ef88e254c99d379bdc
JagathA <jagath.ariy@gmail.com>
2023-03-08 12:45:36 +0000

    Added use context for Misdemeanour

--- client/src/components/fakelandia-router.tsx
+++ client/src/components/fakelandia-router.tsx
@@ -1,20 +1,38 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
-import Confession from './confession/confession-form';
+import ConfessionForm from './confession/confession-form';
 import NotFound from './not_found';
 import MainLayout from './main_layout';
 import MisdemeanourContainer from './misdemeanour-container';
+import React, { createContext } from 'react';
+import { Misdemeanour } from "../types/misdemeanours.types";
+import { useState } from "react";
 
-export const FakeLandiaRouter: React.FC = () =>
-    <>
-        <Routes>
-            <Route path='/' element={<MainLayout />} >
-                <Route index element={<Home />} />
-                <Route path='confession' element={<Confession />} />
-                <Route path='misdemeanour' element={<MisdemeanourContainer />} />
-                <Route path='*' element={<NotFound />} />
-            </Route>
-        </Routes>
-    </>
+const misdemeanour: Misdemeanour[] = [];
+const updateMisdemeanour = (misdameanour: Misdemeanour[]) => { };
 
+export const MisdemeanoursContext = createContext(misdemeanour);
+export const SetMisdemeanoursContext = createContext(updateMisdemeanour)
+
+export const FakeLandiaRouter: React.FC = () => {
+
+    const [misdemeanours, setMisdemeanours] = useState<Array<Misdemeanour>>([]);
+
+    return (
+        <>
+            <MisdemeanoursContext.Provider value={misdemeanours}>
+                <SetMisdemeanoursContext.Provider value={setMisdemeanours}>
+                    <Routes>
+                        <Route path='/' element={<MainLayout />} >
+                            <Route index element={<Home />} />
+                            <Route path='confession' element={<ConfessionForm />} />
+                            <Route path='misdemeanour' element={<MisdemeanourContainer />} />
+                            <Route path='*' element={<NotFound />} />
+                        </Route>
+                    </Routes>
+                </SetMisdemeanoursContext.Provider>
+            </MisdemeanoursContext.Provider>
+        </>
+    )
+}
 export default FakeLandiaRouter;
